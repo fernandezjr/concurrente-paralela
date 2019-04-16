0a30d7ec9431526b567907d12d31dcdc79b9c14e
Rodri <Rodri@DESKTOP-CJOFT41>
2019-04-16 14:05:47 -0300

    Ejercicio terminado

--- Abecedario/Abecedario/Program.cs
+++ Abecedario/Abecedario/Program.cs
@@ -10,7 +10,59 @@ namespace Abecedario
     {
         static void Main(string[] args)
         {
+            Imprimir();
+            Console.ReadLine();
+        }
 
+        static void Imprimir()
+        {
+            Dictionary<char, string> abecedario = new Dictionary<char, string>();
+            int i = 0;
+
+            Task tarea1 = new Task(() =>
+            {
+                while (i < 26)
+                {
+                    if (EsPar(i))
+                    {
+                        abecedario.Add((char)(i + 65), "Tarea 1");
+                        i++;
+                    }
+                }
+            });
+
+            Task tarea2 = new Task(() =>
+            {
+                while (i < 26)
+                {
+                    if (!EsPar(i))
+                    {
+                        abecedario.Add((char)(i + 65), "Tarea 2");
+                        i++;
+                    }
+                }
+            });
+
+            Task tarea3 = new Task(() =>
+            {
+                foreach (KeyValuePair<char, string> par in abecedario)
+                {
+                    Console.WriteLine(par.Key + "   " + par.Value);
+                }
+            });
+
+            tarea1.Start();
+            tarea2.Start();
+
+            Task.WaitAny(new Task[] { tarea1, tarea2 });
+
+            tarea3.Start();
+            tarea3.Wait();            
+        }
+
+        static bool EsPar(int numero)
+        {
+            return (numero % 2 == 0);
         }
     }
 }
